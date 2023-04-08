dfb7159a1ad592fc930d435a35f224f0ae5d9a1f
odinokun <odinokun@gmail.com>
2023-04-08 21:52:55 +0200

    add(layout): change background and ships on level up

--- assets/js/modules/levelCounter.js
+++ assets/js/modules/levelCounter.js
@@ -1,9 +1,12 @@
+import {ChangeLevelDecor} from './changeLevelDecor.js';
+
 const levelCount = document.getElementById('level-count');
 
 export const ChangeLevel = () => {
   if (damage % 5 === 0 && levelCount) {
     ++level
     levelCount.innerHTML = level;
+    ChangeLevelDecor();
   }
 };
 
